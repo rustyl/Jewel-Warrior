ef566b484032919871adcde90f31de42bf4b2986
rustyl <rusty.lev@gmail.com>
2017-01-25 12:00:52 -0800

    yesterdays
    yesterday's work

--- scripts/screen.game.js
+++ scripts/screen.game.js
@@ -3,6 +3,92 @@ jewel.screens["game-screen"] = (function () {
         firstRun = true,
         cursor;
 
+    function selectJewel(x, y) {
+        if (arguments.length === 0) {
+            selectJewel(cursor.x, cursor.y);
+            return;
+        }
+        if (cursor.selected) {
+            var dx = Math.abs(x - cursor.x),
+                dy = Math.abs(y - cursor.y),
+                dist = dx + dy;
+            if (dist === 0) {
+                // deselect the selected jewel
+                setCursor(x,y,false);
+            } else if (dist === 1) {
+                // selected an adjacent one
+                jewel.board.swap(cursor.x, cursor.y,
+                        x,y, playBoardEvents);
+                setCursor(x,y,false);
+            } else {
+                // selected a different jewel
+                setCursor(x, y, true);
+            }
+        } else {
+            setCursor(x, y, true);
+        }
+    }
+
+    function playBoardEvents(events) {
+        var display = jewel.display;
+        if (events.length > 0) {
+            var boardEvent = events.shift(),
+                next = function() {
+                    playBoardEvents(events)
+                };
+            switch (boardEvent.type) {
+                case "more":
+                    display.moveJewels(boardEvent.data, next);
+                    break;
+                case "remove":
+                    display.removeJewels(boardEvent.data, next);
+                    break;
+                case "refill":
+                    display.refill(boardEvent.data, next);
+                    break;
+                default:
+                    next();
+                    break;
+            }
+        } else {
+            display.redraw(jewel.board.getBoard(), function () {
+                // good to go again
+            });
+        }
+    }
+
+    function moveCursor(x,y) {
+        var settings = jewel.settings;
+        if (cursor.selected) {
+            x += cursor.x;
+            y += cursor.y;
+            if (x >= 0 && x < settings.cols &&
+                y >= 0 && y < settings.rows) {
+                selectJewel(x,y);
+            }
+        } else {
+            x = (cursor.x + x + settings.cols) % settings.cols;
+            y = (cursor.y + y + settings.rows) % settings.rows;
+            setCursor(x,y,false);
+        }
+    }
+
+    function moveUp() {
+        moveCursor(0,-1);
+    }
+
+    function moveDown() {
+        moveCursor(0,1);
+    }
+
+    function moveLeft() {
+        moveCursor(-1,0);
+    }
+
+    function moveRight() {
+        moveCursor(1,0);
+    }
+
     function exitGame() {
         pauseGame();
         var confirmed = window.confirm(
